d11d7542d7d8d5668405fb7bc02ea53376f4c0c8
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-02-20 12:43:42 +0300

    minor change

---
+++ Board.h
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Board {
+    private :
+        int width = 23 ;
+        int height = 11 ;
+    public :
+        void draw(){
+            for (int i = 0; i < width; i++)
+            {
+                std::cout << '#' ;
+            }
+            std::cout << std::endl ;
+            for (int i = 0; i < height; i++){
+                for (int j = 0; j < width; j++){
+                    if (j == 0 || j == 22){
+                        std::cout << '#' ;
+                    }else{
+                        std::cout << ' ' ;
+                    }
+                }
+                std::cout << std::endl ; 
+            }
+            for (int i = 0; i < width; i++)
+            {
+                std::cout << '#' ;
+            }
+            
+        }
+};
