90a7f2cbe65311dc95bc8f006d5bb367c048c159
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-02-21 12:50:32 +0300

    added header

---
+++ Player.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+
+class Player{
+    private:
+        char symbol = '*';
+
+    public:
+        int playerX = ( rand() % 10 ) + 1 ;
+        int playerY = ( rand() % 22 ) + 1 ;
+
+        void move(){
+            int action = getch();
+            switch (action)
+            {
+            case 'w':
+                playerY-- ;
+                break;
+            
+            case 's':
+                playerY++ ;
+                break;
+            
+            case 'd':
+                playerX++;
+                break ;
+            
+            case 'a':
+                playerX--;
+                break;
+            
+            default:
+                break;
+            }
+
+        };
+};
--- snake.cpp
+++ snake.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
-#include <conio.h>
+#include "Player.h"
 #include "Board.h"
 bool gameOver = false ;
-
-class Player{
-    private:
-        char symbol = '*';
-
-    public:
-        int playerX = ( rand() % 10 ) + 1 ;
-        int playerY = ( rand() % 22 ) + 1 ;
-
-        void move(){
-            int action = getch();
-            switch (action)
-            {
-            case 'w':
-                playerY-- ;
-                break;
-            
-            case 's':
-                playerY++ ;
-                break;
-            
-            case 'd':
-                playerX++;
-                break ;
-            
-            case 'a':
-                playerX--;
-                break;
-            
-            default:
-                break;
-            }
-
-        };
-};
-
 
 int main(){
     Board myboard ;
     Player myplayer ;
+
     while (!gameOver)
     {
         myboard.draw(myplayer.playerX, myplayer.playerY);
         myplayer.move();
+        Sleep(100);
     }
     
     return 0 ;
