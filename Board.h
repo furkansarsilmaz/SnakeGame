76ca1b585fa3b7e9ac642faa080f449f854e4c68
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-02-21 12:42:44 +0300

    added move

--- Board.h
+++ Board.h
@@ -5,18 +5,32 @@ class Board {
         int width = 23 ;
         int height = 11 ;
     public :
-        void draw(){
+        void draw(int playerX, int playerY){
+            system("cls");
             for (int i = 0; i < width; i++)
             {
                 std::cout << '#' ;
             }
             std::cout << std::endl ;
-            for (int i = 0; i < height; i++){
-                for (int j = 0; j < width; j++){
-                    if (j == 0 || j == 22){
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (j == 0 || j == 22)
+                    {
                         std::cout << '#' ;
-                    }else{
-                        std::cout << ' ' ;
+                    }
+                    else
+                    {
+                        if (playerX == j && playerY == i)
+                        {
+                            std::cout << '*' ;
+                        }
+                        else
+                        {
+                            std::cout << ' ' ;
+                        }
+                        
                     }
                 }
                 std::cout << std::endl ; 
--- snake.cpp
+++ snake.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <cstdlib>
+#include <windows.h>
+#include <conio.h>
 #include "Board.h"
+bool gameOver = false ;
+
+class Player{
+    private:
+        char symbol = '*';
+
+    public:
+        int playerX = ( rand() % 10 ) + 1 ;
+        int playerY = ( rand() % 22 ) + 1 ;
+
+        void move(){
+            int action = getch();
+            switch (action)
+            {
+            case 'w':
+                playerY-- ;
+                break;
+            
+            case 's':
+                playerY++ ;
+                break;
+            
+            case 'd':
+                playerX++;
+                break ;
+            
+            case 'a':
+                playerX--;
+                break;
+            
+            default:
+                break;
+            }
+
+        };
+};
+
 
 int main(){
-    Board board;
-    board.draw();
+    Board myboard ;
+    Player myplayer ;
+    while (!gameOver)
+    {
+        myboard.draw(myplayer.playerX, myplayer.playerY);
+        myplayer.move();
+    }
+    
     return 0 ;
 }
